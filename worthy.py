49b3c9ff617116afde617c69fe6ec9c556329bf3
meklenbpo <yk@kantynent.com>
2020-05-09 17:45:59 +0300

    initial command loop

---
+++ worthy.py
@@ -0,0 +1,29 @@
+"""
+Worthy CLI app.
+"""
+
+import pandas as pd
+
+
+def decorate(s:str) -> None:
+	print('')
+	print(s)
+	for _ in range(len(s)):
+		print('-', end='')
+	print('\n')
+
+
+greeting = 'Welcome to Worthy CLI:'
+decorate(greeting)
+
+
+
+while True:
+	cmd = input('$>')
+	cmd = cmd.lower()
+	if cmd == 'exit':
+		break
+
+
+print('---\nTerminated successfully.\n')
+# EOA
